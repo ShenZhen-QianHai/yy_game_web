a53dddff221a082ddc1dd3f3e6b767afc7975e68
Mr.Li <1752146907@qq.com>
2024-12-18 21:53:31 +0800

    登陆页面布局

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,12 +2,36 @@ import React from 'react'
 import logincss from './index.module.css';
 import logo from "../../images/logo.png"
 import icon2 from "../../images/icon2.png"
+import icon4 from "../../images/icon4.png"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
+import { useNavigate } from 'react-router-dom';
 
 function About() {
+    const navigate = useNavigate();
+    const handleLogin = () => { 
+    }
+    const handleForget = () => { 
+    }
+    const handleCreate = () => { 
+    }
+
     return (
         <div className={`${logincss.box}`}>
+        <div className={`${logincss.box_top}`}>
+            <div style={{position: 'relative'}}> 
+                <img
+                    onClick={() => {
+                        navigate('/');
+                    }}
+                    src={icon4}
+                    alt=""
+                    className={`${logincss.main_left_img}`}
+                /> 
+                Log in
+            </div>
+        </div>
+
             <img className={`${logincss.logo}`} src={logo} alt="" />
             <div className={`${logincss.main}`}>
                 <div className={`${logincss.main_left}`}> 
@@ -36,12 +60,13 @@ function About() {
                             <div className={`${logincss.main_right_from_label}`}>Password<span>*</span></div>
                             <input className={`${logincss.main_right_from_input}`} type="text" placeholder="Please enter your password" />
                         </div>
-                        <div className={`${logincss.main_right_from_submit}`}>Log in</div>
-                        <div className={`${logincss.main_right_from_item}`}>
+                        <div className={`${logincss.main_right_from_submit}`}
+                                    onClick={handleLogin}>Log in</div>
+                        <div className={`${logincss.main_right_from_item} ${logincss.main_right_from_item2}`}>
                             <div className={`${logincss.main_right_from_label}`}></div>
                             <div className={`${logincss.main_right_from_footer}`}>
-                                <div className={`${logincss.main_right_from_footer_1}`}>Forget password?</div>
-                                <div className={`${logincss.main_right_from_footer_2}`}>Create an account</div>
+                                <div className={`${logincss.main_right_from_footer_1}`} onClick={handleForget}>Forget password?</div>
+                                <div className={`${logincss.main_right_from_footer_2}`} onClick={handleCreate}>Create an account</div>
                             </div>
                         </div>
                     </div>
